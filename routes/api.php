7a42fdf3d81550f33b30b22ec11227ca0152d493
Xhyrone Malapote <malapotexhyrone@gmail.com>
2026-09-06 18:52:35 +0800

    Complete Week 6 Sanctum authentication

--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,43 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\EmployeeController;
 
-/// Week 2 - Student API
+// Week 2 - Student API
 Route::get('/students', function () {
     return response()->json([
         [
             'id' => 1,
             'name' => 'Juan Dela Cruz',
-            'course' => 'BSIT'
+            'course' => 'BSIT',
         ],
         [
             'id' => 2,
             'name' => 'Maria Santos',
-            'course' => 'BSIT'
-        ]
+            'course' => 'BSIT',
+        ],
     ]);
 });
 
-// Week 4 - Employee CRUD API
-Route::get('/employees', [EmployeeController::class, 'index']);
+// Week 6 - Public Authentication Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::post('/employees', [EmployeeController::class, 'store']);
+// Week 6 - Protected Routes
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::get('/employees/{id}', [EmployeeController::class, 'show']);
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
 
-Route::put('/employees/{id}', [EmployeeController::class, 'update']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+    // Protected Employee API
+    Route::get('/employees', [EmployeeController::class, 'index']);
+    Route::post('/employees', [EmployeeController::class, 'store']);
+    Route::get('/employees/{id}', [EmployeeController::class, 'show']);
+    Route::put('/employees/{id}', [EmployeeController::class, 'update']);
+    Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+});
